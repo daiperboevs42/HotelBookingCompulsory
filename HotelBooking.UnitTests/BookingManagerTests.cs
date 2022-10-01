96d11dc09dd42ea34e4cfa02f486e90ca64d6066
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2022-10-01 10:55:25 +0200

    hhh

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -1,6 +1,7 @@
 using System;
 using HotelBooking.Core;
 using HotelBooking.UnitTests.Fakes;
+using Moq;
 using Xunit;
 
 namespace HotelBooking.UnitTests
@@ -8,6 +9,8 @@ namespace HotelBooking.UnitTests
     public class BookingManagerTests
     {
         private IBookingManager bookingManager;
+        private readonly Mock<IRepository<Booking>> _mockBookingRepo;
+        private readonly Mock<IRepository<Room>> _mockRoomRepo;
 
         public BookingManagerTests(){
             DateTime start = DateTime.Today.AddDays(10);
@@ -15,6 +18,8 @@ namespace HotelBooking.UnitTests
             IRepository<Booking> bookingRepository = new FakeBookingRepository(start, end);
             IRepository<Room> roomRepository = new FakeRoomRepository();
             bookingManager = new BookingManager(bookingRepository, roomRepository);
+            _mockRoomRepo = new Mock<IRepository<Room>>();
+            _mockBookingRepo = new Mock<IRepository<Booking>>();
         }
 
         [Fact]
@@ -40,6 +45,60 @@ namespace HotelBooking.UnitTests
             // Assert
             Assert.NotEqual(-1, roomId);
         }
+
+        [Fact]
+        public void BookingManager_WithNullRepo_ShouldThrowException()
+        {
+            var bookingManager = new BookingManager(_mockBookingRepo.Object, _mockRoomRepo.Object);
+            var action = new Action(() => new BookingManager(null, null));
+            throw new NullReferenceException("Require a Repo");
+
+            //Action action = () => new BookingManager(null, null);
+            //Throw<NullReferenceException>().WithMessage("Requires a Repo");
+
+        }
+
+
+        [Fact]
+        public void FullyOccupiedDates_StartDateIsLaterThanEndDate_ThrowArgumentException()
+        {
+            //Arrange
+            var bookingManager = new BookingManager(_mockBookingRepo.Object, _mockRoomRepo.Object);
+            DateTime startDate = DateTime.Today.AddDays(5);
+            DateTime endDate = DateTime.Today.AddDays(2);
+            Action act = () => bookingManager.GetFullyOccupiedDates(startDate, endDate);
+            //Act
+            var rec = Record.Exception(act);
+            //Assert
+            Assert.IsType<ArgumentException>(rec);
+        }
+
+
+        [Fact]
+        public void CreateBooking_IncorrectStartDate_ReturnFalse()
+        {
+            //Arrange
+            Booking newBooking = new()
+            {
+                RoomId = 3,
+                StartDate = DateTime.Today.AddDays(14),
+                EndDate = DateTime.Today.AddDays(14),
+                IsActive = false,
+                CustomerId = 2,
+                Customer = 
+                new Customer() { Email = "dggg@mail", Id = 1, Name = "Everett"},
+                Room = new Room() { Description ="Comfrtoab le" }
+            };
+
+            //Act
+            bool isCreated = bookingManager.CreateBooking(newBooking);
+
+            //Assert
+            Assert.False(isCreated);
+
+
+        }
+
 
     }
 }
